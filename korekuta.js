a0abb31a724d8caf73ee59949ad6e943c3f99793
manami.project <manami.project@posteo.net>
2018-07-22 22:16:52 +0200

    Stable version of ANILIST

--- korekuta.js
+++ korekuta.js
@@ -1,11 +1,10 @@
 'use strict';
 
+const MAX_TIMEOUT = 10000
 const puppeteer = require('puppeteer');
 
 var express = require('express')
 var app = express()
-
-
 
 
 app.get('/:distributor/:id', async function (req, res) {
@@ -15,12 +14,21 @@ app.get('/:distributor/:id', async function (req, res) {
       '--disable-setuid-sandbox'
     ]
   });
+
   const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(MAX_TIMEOUT)
+
+  process.on("unhandledRejection", (reason, p) => {
+    console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
+  });
 
   switch(req.params.distributor) {
     case 'anilist':
-    await page.goto('https://anilist.co/anime/' + req.params.id);
-    break;
+      await page.goto('https://anilist.co/anime/' + req.params.id)
+        .catch(error => { console.log('rejection in goto', error.message); });
+      await page.waitFor('h1', {'timeout': MAX_TIMEOUT})
+        .catch(error => { console.log('rejection in goto', error.message); });
+      break;
     case 'kitsu':
       await page.goto('https://kitsu.io/anime/' + req.params.id);
       await page.waitFor('h3');
@@ -30,14 +38,13 @@ app.get('/:distributor/:id', async function (req, res) {
     case 'anidb':
       await page.goto('https://anidb.net/a' + req.params.id);
     default:
-      res.sendStatus(404)
+      res.statusCode = 404
   }
   
-  
   let html = await page.evaluate(() => document.body.innerHTML);
+  browser.close();
   res.end(html)
 });
-
 
 
 app.listen(3000)
